f7473db426e2ca15cdd952ff3023f9aea47bb7a9
Norman Yadev <normanyadev00@gmail.com>
2024-12-31 14:12:40 +0530

    updated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-# Load the dataset
+# Loading bank-full dataset
 file_path = "C:\\Users\\Muralish\\Desktop\\Machine lerning coursework\\Data\\bank\\bank-full.csv"
 data = pd.read_csv(file_path, sep=";")  # creates dataframe
 
@@ -22,9 +22,19 @@ for col in binary_cols:
 data = pd.get_dummies(data, columns=['job', 'marital', 'education', 'contact', 'month', 'poutcome'])
 data = data.astype(int)
 
+data = data.drop(columns=['duration'])
+
+# Create the 'was_contacted' column [0 means not contacted, 1 means contacted]
+data['was_contacted'] = (data['pdays'] != -1).astype(int)
+
+# Step 2: Replace -1 in 'pdays' with NaN
+data['pdays'] = data['pdays'].replace(-1, np.nan)
+
+# Remove outlier from previous column manually
+data = data[data['previous'] != 275]
 
 
 # Save the preprocessed data
-preprocessed_file_path = "C:\\Users\\Muralish\\Desktop\\Machine lerning coursework\\Data\\bank\\preprocessed_bankdata\\bank-full\\bank_preprocessed3.csv"
+preprocessed_file_path = "C:\\Users\\Muralish\\Desktop\\Machine lerning coursework\\Data\\bank\\preprocessed_bankdata\\bank-full\\bank_preprocessed1.csv"
 data.to_csv(preprocessed_file_path, index=False)
 print(f"Preprocessed data saved to {preprocessed_file_path}")
