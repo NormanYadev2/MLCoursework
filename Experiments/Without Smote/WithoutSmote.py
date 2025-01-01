37344e50211c3683a4df04eea7ef1440d1a63ecb
Norman Yadev <normanyadev00@gmail.com>
2025-01-01 09:49:24 +0530

    update

--- Experiments/Without Smote/WithoutSmote.py
+++ Experiments/Without Smote/WithoutSmote.py
@@ -1,6 +1,4 @@
-import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
@@ -10,37 +8,10 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import RandomizedSearchCV
 
-
-# Load the preprocessed data
-preprocessed_file_path = "C:\\Users\\Muralish\\Desktop\\Machine lerning coursework\\Data\\bank\\preprocessed_bankdata\\bank-full\\bank_preprocessed1.csv"
-data = pd.read_csv(preprocessed_file_path)
-
-# change NaN values to mean for normalization
-data['pdays'] = data['pdays'].fillna(data['pdays'].mean())
-
-
-numeric_cols = ['age', 'pdays', 'previous', 'balance', 'campaign', 'day']
-
-
-# Scale numeric features
-scaler = StandardScaler()
-data[numeric_cols] = scaler.fit_transform(data[numeric_cols])
-
-# Applying PCA for dimensionality reduction
-pca = PCA(n_components=0.95)  # Retain 95% variance
-X_pca = pca.fit_transform(data[numeric_cols])
-
-# Create a new DataFrame for PCA-transformed features
-pca_columns = [f'PCA_{i+1}' for i in range(X_pca.shape[1])]
-pca_data = pd.DataFrame(X_pca, columns=pca_columns)
-
-# Combining PCA data with other columns except numeric_cols
-final_data = pd.concat([pca_data, data.drop(columns=numeric_cols)], axis=1)
-
-# Handling missing values in the target variable
-final_data['y'] = final_data['y'].fillna(final_data['y'].mode()[0])  # Fill NaNs with the mode
+from FeatureEngineering import final_data, pca
 
 # Save the finalized dataset
+
 finalized_file_path = "C:\\Users\\Muralish\\Desktop\\Machine lerning coursework\\Data\\bank\\preprocessed_bankdata\\bank-full\\bank_finalized1.csv"
 final_data.to_csv(finalized_file_path, index=False)
 print(f"Finalized data saved to {finalized_file_path}")
